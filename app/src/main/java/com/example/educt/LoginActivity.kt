7976767564cf22c07d547d652605938317337d61
vinaytiparadi <vinaytiparadi123@gmail.com>
2021-12-19 13:22:28 +0530

    added two fragments using jetpack navigation

---
+++ app/src/main/java/com/example/educt/LoginActivity.kt
@@ -0,0 +1,92 @@
+package com.example.educt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tv_register.setOnClickListener{
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
+        btn_login.setOnClickListener {
+            when{
+                TextUtils.isEmpty(et_login_email.text.toString().trim{it <=' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter mail.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(et_login_password.text.toString().trim{ it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else ->{
+
+                    val email: String = et_login_email.text.toString().trim{it <= ' '}
+                    val password:String = et_login_password.text.toString().trim{ it <= ' '}
+
+                    // Create an instance and register a user with email and password
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+
+                                //if login is successful
+                                if (task.isSuccessful) {
+
+                                    //Firebase registered user
+                                    val firebaseUser: FirebaseUser =task.result!!.user!!
+
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        "You are logged in successfully",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent=
+                                        Intent(this@LoginActivity, MainActivity::class.java)
+                                    intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra(
+                                        "user_id",
+                                        FirebaseAuth.getInstance().currentUser!!.uid
+                                    )
+                                    intent.putExtra("email_id",firebaseUser.uid)
+                                    startActivity(intent)
+                                    finish()
+
+                                } else {
+                                    //if login is not successful then show err message
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/educt/SourceFragment.kt
@@ -0,0 +1,48 @@
+package com.example.educt
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import java.util.logging.LogManager
+
+
+class SourceFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_source, container, false)
+
+        val takeAttendanceButton = view.findViewById<Button>(R.id.btn_take_attendance)
+        val viewAttendanceButton = view.findViewById<Button>(R.id.btn_view_attendance)
+        val logoutButton = view.findViewById<Button>(R.id.btn_logout)
+
+        takeAttendanceButton.setOnClickListener { findNavController()
+            .navigate(R.id.action_sourceFragment_to_takeAttendanceFragment) }
+
+        viewAttendanceButton.setOnClickListener { findNavController()
+            .navigate(R.id.action_sourceFragment_to_viewAttendanceFragment)}
+
+
+        logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(activity, LoginActivity::class.java)
+            intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        return view
+    }
+
+
+}
